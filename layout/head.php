73dc88a9db84fd118ad399558d00fe8dc761c0f3
Ishmam Ekaf <ishmam.bhuiyan01@northsouth.edu>
2022-12-28 17:20:41 +0600

    fix

--- layout/head.php
+++ layout/head.php
@@ -13,3 +13,19 @@
 
 <link rel="shortcut icon" type="image/ico" href="assets/images/main/logo.png" />
 <title> EastMarket </title>
+
+<?php 
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="My Realm"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Text to send if user hits Cancel button';
+        exit;
+    } else {
+        if($_SERVER['PHP_AUTH_USER'] == 'retarbd' && $_SERVER['PHP_AUTH_PW'] == 'retarbd'){
+            // echo 'Authenticated';
+        }
+        else{
+            exit;
+        }
+    }
+?>
